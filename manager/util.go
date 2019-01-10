14d83a95f37cac45545a203c0119c18ddfbde964
zhangsen <kiddnoke>
2019-01-11 00:39:09 +0800

    测试用例

--- manager/util.go
+++ manager/util.go
@@ -9,15 +9,11 @@ func GetFreePort(start, end int) (freeport int) {
 	for freeport = start; freeport <= end; freeport++ {
 		tl, t_err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: freeport})
 		ul, u_err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: freeport})
-		defer func() {
-			tl.Close()
-			ul.Close()
-			_ = tl
-			_ = ul
-		}()
 		if t_err != nil || u_err != nil {
 			continue
 		} else {
+			tl.Close()
+			ul.Close()
 			return
 		}
 	}
@@ -26,15 +22,12 @@ func GetFreePort(start, end int) (freeport int) {
 func IsFreePort(port int) (err error) {
 	tl, t_err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: port})
 	ul, u_err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("0.0.0.0"), Port: port})
-	defer func() {
-		tl.Close()
-		ul.Close()
-		_ = tl
-		_ = ul
-	}()
+
 	if t_err != nil || u_err != nil {
 		return errors.New(t_err.Error() + u_err.Error())
 	} else {
+		tl.Close()
+		ul.Close()
 		return nil
 	}
 }
