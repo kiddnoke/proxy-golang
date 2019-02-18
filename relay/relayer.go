cd0f86845a25c26dc59743b7b119ee5180a52736
zhangsen <kiddnoke>
2019-02-18 14:20:45 +0800

    修改tcp 和udp 转发器作为公共成员变量

--- relay/relayer.go
+++ relay/relayer.go
@@ -6,31 +6,31 @@ type ProxyRelayer interface {
 	Close()
 }
 type ProxyRelay struct {
-	t *TcpRelay
-	u *UdpRelay
+	*TcpRelay
+	*UdpRelay
 	*proxyinfo
 }
 
 func NewProxyRelay(p *proxyinfo) (r *ProxyRelay, err error) {
 	t, err1 := NewTcpRelayByProxyInfo(p)
 	u, _ := NewUdpRelayByProxyInfo(p)
-	return &ProxyRelay{t: t, u: u, proxyinfo: p}, err1
+	return &ProxyRelay{TcpRelay: t, UdpRelay: u, proxyinfo: p}, err1
 }
 func (r *ProxyRelay) Start() {
 	if r.running == false {
 		r.proxyinfo.Logger.Printf("ProxyRelay Start")
-		r.t.Start()
-		r.u.Start()
+		r.TcpRelay.Start()
+		r.UdpRelay.Start()
 	}
 }
 func (r *ProxyRelay) Stop() {
 	r.Printf("ProxyRelay Stop")
-	r.t.Stop()
-	r.u.Stop()
+	r.TcpRelay.Stop()
+	r.UdpRelay.Stop()
 }
 func (r *ProxyRelay) Close() {
 	r.Printf("ProxyRelay Close")
 	r.Stop()
-	r.t.Close()
-	r.u.Close()
+	r.TcpRelay.Close()
+	r.UdpRelay.Close()
 }
