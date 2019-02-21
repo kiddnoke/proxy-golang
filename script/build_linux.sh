07a1f21dc0b072ba425b0dcbb907a822d616321d
zhangsen <kiddnoke>
2019-02-21 14:33:33 +0800

    修改编译脚本

--- script/build_linux.sh
+++ script/build_linux.sh
@@ -5,7 +5,7 @@ export CGO_ENABLED=0
 
 COMMIT_HASH=`git rev-parse HEAD 2>/dev/null`
 BUILD_DATE=`date  +%Y-%m-%d-%H:%M`
-TARGET=../../bin/vpnedge_linux
+TARGET=../../bin/vpnedge_linux_release
 SOURCE=../cmd/VpnEdge
 cd ${SOURCE}
 go build -ldflags "-X \"main.BuildVersion=${COMMIT_HASH}\" -X \"main.BuildDate=${BUILD_DATE}\"" -i -o ${TARGET}
--- script/build_win.sh
+++ script/build_win.sh
@@ -3,7 +3,7 @@
 COMMIT_HASH=`git rev-parse HEAD 2>/dev/null`
 BUILD_DATE=`date  +%Y-%m-%d-%H:%M`
 
-TARGET=../../bin/vpnedge.exe
+TARGET=../../bin/vpnedge_win_release.exe
 SOURCE=../cmd/VpnEdge
 cd ${SOURCE}
 go build -ldflags "-X \"main.BuildVersion=${COMMIT_HASH}\" -X \"main.BuildDate=${BUILD_DATE}\"" -i -o ${TARGET}
