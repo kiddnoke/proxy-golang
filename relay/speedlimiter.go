3bf3a33741f255ab9417fb104f431207c53df19e
zhangsen <kiddnoke>
2019-02-25 16:44:34 +0800

    fix bug :integer divide by zero

--- relay/speedlimiter.go
+++ relay/speedlimiter.go
@@ -33,7 +33,7 @@ func (s *Limiter) SetLimit(bytesPerSec int) {
 	s.Limiter = rate.NewLimiter(rate.Limit(bytesPerSec), burstsize)
 }
 func (s *Limiter) WaitN(n int) (err error) {
-	if err = s.Limiter.WaitN(s.ctx, n); err != nil {
+	if err = s.Limiter.WaitN(s.ctx, n); err != nil && s.Limiter.Burst() != 0 {
 		sleepDuration := n * 1000 / s.Limiter.Burst()
 		time.Sleep(time.Duration(sleepDuration) * time.Millisecond)
 	}
