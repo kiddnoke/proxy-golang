2276a368a4b4f3f9f987d9fe5c4163ad3be84dc8
zhangsen <kiddnoke>
2019-02-25 19:16:17 +0800

    更新编译脚本

--- script/build_linux.sh
+++ script/build_linux.sh
@@ -8,7 +8,7 @@ COMMIT_HASH=`git rev-parse HEAD 2>/dev/null`
 BUILD_DATE=`date  +%Y-%m-%d-%H:%M`
 BRANCH_NAME=`git symbolic-ref --short -q HEAD`
 
-TARGET=../../bin/vpnedge_${OS}
+TARGET=../../bin/vpnedge_${OS}_${BRANCH_NAME}
 SOURCE=../cmd/VpnEdge
 cd ${SOURCE}
 go build -ldflags "-X \"main.BuildVersion=${COMMIT_HASH}\" -X \"main.BuildDate=${BUILD_DATE}\" -X \"main.BuildBranch=${BRANCH_NAME}\"" -i -o ${TARGET}
