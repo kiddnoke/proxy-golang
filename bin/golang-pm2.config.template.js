dd8aeb98f7c288d82c85d412a790c89104df57cc
zhangsen <kiddnoke>
2019-02-28 11:31:08 +0800

    pm2 启动脚本

--- bin/golang-pm2.config.template.js
+++ bin/golang-pm2.config.template.js
@@ -1,48 +1,55 @@
+/*
+ Country string 国家
+ AreaCode int 区域
+ LEVEL string
+ VpnMgrCore string 中心服务器地址 ip:port
+ COUNTER_URL string 统计后台地址 ip:port
+ */
 module.exports = {
     apps: [
         {
-            name: "IggAgent_COUNTRY",
+            name: "IggAgent_Country_AreaCode_LEVEL",
             cwd: "IggAgent",
             script: "IggAgent_linux",
             args: ['-url', 'COUNTER_URL', '-size', 100,],
-            error_file: "/data/vpnedge/logs/IggAgent_COUNTRY.err.log",
-            out_file: "/data/vpnedge/logs/IggAgent_COUNTRY.out.log",
+            error_file: "/data/vpnedge/logs/IggAgent_Country_LEVEL.err.log",
+            out_file: "/data/vpnedge/logs/IggAgent_Country_LEVEL.out.log",
         },
         {
-            name: "ssEdge_COUNTRY_1",
+            name: "ssEdge_Country_AreaCode_LEVEL_1",
             cwd: "VpnApp",
             script: "vpnedge_linux",
-            args: ['-url', '47.74.157.30:7001', '-Id', 1, '-beginport', 20000, '-endport', 20999,
-                '-state', 'COUNTRY', '-area', '1'],
-            error_file: "/data/vpnedge/logs/ssEdge_COUNTRY_1.err.log",
-            out_file: "/data/vpnedge/logs/ssEdge_COUNTRY_1.out.log",
+            args: ['-url', 'VpnMgrCore', '-Id', 1, '-beginport', 20000, '-endport', 29999,
+                '-state', 'Country', '-area', 'AreaCode'],
+            error_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_1.err.log",
+            out_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_1.out.log",
         },
         {
-            name: "ssEdge_COUNTRY_2",
+            name: "ssEdge_Country_AreaCode_LEVEL_2",
             cwd: "VpnApp",
             script: "vpnedge_linux",
-            args: ['-url', '47.74.157.30:7001', '-Id', 2, '-beginport', 21000, '-endport', 21999,
-                '-state', 'COUNTRY', '-area', '2'],
-            error_file: "/data/vpnedge/logs/ssEdge_COUNTRY_2.err.log",
-            out_file: "/data/vpnedge/logs/ssEdge_COUNTRY_2.out.log",
+            args: ['-url', 'VpnMgrCore', '-Id', 2, '-beginport', 30000, '-endport', 39999,
+                '-state', 'Country', '-area', 'AreaCode'],
+            error_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_2.err.log",
+            out_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_2.out.log",
         },
         {
-            name: "ssEdge_COUNTRY_3",
+            name: "ssEdge_Country_AreaCode_LEVEL_3",
             cwd: "VpnApp",
             script: "vpnedge_linux",
-            args: ['-url', '47.74.157.30:7001', '-Id', 3, '-beginport', 22000, '-endport', 22999,
-                '-state', 'COUNTRY', '-area', '3'],
-            error_file: "/data/vpnedge/logs/ssEdge_COUNTRY_3.err.log",
-            out_file: "/data/vpnedge/logs/ssEdge_COUNTRY_3.out.log",
+            args: ['-url', 'VpnMgrCore', '-Id', 3, '-beginport', 40000, '-endport', 49999,
+                '-state', 'Country', '-area', 'AreaCode'],
+            error_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_3.err.log",
+            out_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_3.out.log",
         },
         {
-            name: "ssEdge_COUNTRY_4",
+            name: "ssEdge_Country_AreaCode_LEVEL_4",
             cwd: "VpnApp",
             script: "vpnedge_linux",
-            args: ['-url', '47.74.157.30:7001', '-Id', 4, '-beginport', 23000, '-endport', 23999,
-                '-state', 'COUNTRY', '-area', '4'],
-            error_file: "/data/vpnedge/logs/ssEdge_COUNTRY_4.err.log",
-            out_file: "/data/vpnedge/logs/ssEdge_COUNTRY_4.out.log",
+            args: ['-url', 'VpnMgrCore', '-Id', 4, '-beginport', 50000, '-endport', 59999,
+                '-state', 'Country', '-area', 'AreaCode'],
+            error_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_4.err.log",
+            out_file: "/data/vpnedge/logs/ssEdge_Country_AreaCode_LEVEL_4.out.log",
         }
     ]
 };
